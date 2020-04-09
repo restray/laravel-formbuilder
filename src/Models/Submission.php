3310a41985b5e5436d71d560c3c078a7a5623fce
Restray <contact@restray.org>
2020-04-09 19:19:33 +0200

    Patch unknown helper on Submission

--- src/Models/Submission.php
+++ src/Models/Submission.php
@@ -174,7 +174,7 @@ class Submission extends Model
         }
 
         if ($limit_string) {
-            $str = str_limit($str, 20);
+            $str = \Illuminate\Support\Str::limit($str, 20, '...');
         }
 
         // if the type is 'file' then we have to render this as a link
